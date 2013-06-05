6954fe9fa9c0c917602def2cf3b2e367caec3fe0
Joshua Hoblitt <jhoblitt@cpan.org>
2013-06-05 14:57:33 -0700

    add gcc-c++ & rpm-build packages to gpfs::kernel_builder

--- manifests/kernel_builder.pp
+++ manifests/kernel_builder.pp
@@ -1,6 +1,8 @@
 class gpfs::kernel_builder  {
 
-  package{['imake', 'kernel-devel', 'kernel-headers']:
+  # the gpfs 3.5 kernel glue build scripts check for the existance of g++ and
+  # fail if it's not present
+  package{['imake', 'kernel-devel', 'kernel-headers', 'gcc-c++', 'rpm-build']:
     ensure  => present,
   }
 
