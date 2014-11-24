d326b27ac2cf3c2e4109c03c752025b309e72acf
Joshua Hoblitt <jhoblitt@cpan.org>
2014-11-24 13:10:27 -0700

    add gpfs::v340 & v350 classes
    To replace gpfs::340 & gpfs::350 as class names that start with a number
    will not be support by puppet 4.x

--- manifests/340.pp
+++ manifests/340.pp
@@ -1,3 +1,4 @@
+# XXX class name is puppet 4.x incompatible; replace with gpfs::v340
 class gpfs::340 {
   class {'gpfs::install' : gpfs_version => '3.4.0' }
 }
---
+++ manifests/v350.pp
@@ -0,0 +1,3 @@
+class gpfs::v350 {
+  class {'gpfs::install' : gpfs_version => '3.5.0' }
+}
