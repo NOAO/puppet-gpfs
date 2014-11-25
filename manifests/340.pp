c5f4b2388f4fc87951091aa42ef1ecc43cfad992
Joshua Hoblitt <jhoblitt@cpan.org>
2014-11-25 10:42:38 -0700

    remove gpfs::340 & gpfs::350 classes

--- manifests/340.pp
+++
@@ -1,4 +0,0 @@
-# XXX class name is puppet 4.x incompatible; replace with gpfs::v340
-class gpfs::340 {
-  class {'gpfs::install' : gpfs_version => '3.4.0' }
-}
--- manifests/350.pp
+++
@@ -1,4 +0,0 @@
-# XXX class name is puppet 4.x incompatible; replace with gpfs::v350
-class gpfs::350 {
-  class {'gpfs::install' : gpfs_version => '3.5.0' }
-}
